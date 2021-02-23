22189880ead5cfc97d9292c841c195322e6feb0b
geekbim <manyuabim9@gmail.com>
2021-02-23 07:56:12 +0700

    learn docker

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt",
+	"fmt"
 	"net/http"
 )
 
@@ -10,5 +10,5 @@ func main() {
 		fmt.Fprintf(w, "Hello World")
 	})
 
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8080", nil)
 }
